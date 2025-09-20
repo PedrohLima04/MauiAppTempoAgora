64a478b4fcb5dd741e30c5ba0d65b577d5db8256
PedroL <pedrophlima04@gmail.com>
2025-09-20 13:54:25 -0300

    alerta de sem conexão a internet, conexão com api,  saída dos dados coletados da api.

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,24 +1,64 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+            {
+                await DisplayAlert("Sem conexão", "Verifique sua internet e tente novamente.", "OK");
+                return; 
+            }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
+
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Por do Sol: {t.sunset} \n" +
+                                         $"Temp Máx: {t.temp_max} \n" +
+                                         $"Temp Min: {t.temp_min} \n" +
+                                         $"Descrição: {t.description} \n" +
+                                         $"Visibilidade: {t.visibility} \n" +
+                                         $"Velocidade do Vento: {t.speed} \n";
+
+                        lbl_res.Text = dados_previsao;
+
+                    }
+                    else
+                    {
+
+                        lbl_res.Text = "Cidade não encontrada";
+                        await DisplayAlert("Cidade não encontrada", "Digite o nome correto da Cidade", "OK");
+                    }
+
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
 
